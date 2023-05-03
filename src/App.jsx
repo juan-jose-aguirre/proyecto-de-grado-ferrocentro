d9dca026e81a92bc39acf496c21dec28f9cdffcc
Juan <juanjoa04@gmail.com>
2023-05-03 15:39:58 -0500

    creacion componente navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from './componentes/NavBar';
+
+function App() {
+  return (
+    <NavBar>
+    </NavBar>
+  );
+}
+
+export default App;
